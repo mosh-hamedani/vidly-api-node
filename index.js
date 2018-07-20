e727256d0f4f9cb50fd12e860bbdf04c798637e7
Moshfegh Hamedani <moshfegh@live.com.au>
2018-07-20 10:02:48 -0700

    Move the port to config.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const winston = require("winston");
 const express = require("express");
+const config = require("config");
 const app = express();
 
 require("./startup/logging")();
@@ -9,7 +10,7 @@ require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
