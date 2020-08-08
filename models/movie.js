a9c1cd58aa44b5314502ea489bcdf9be255f98ff
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2020-08-08 13:42:49 +0200

    update dependencies and fix joi validation error

--- models/movie.js
+++ models/movie.js
@@ -1,43 +1,46 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const {genreSchema} = require('./genre');
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const { genreSchema } = require("./genre");
 
-const Movie = mongoose.model('Movies', new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true, 
-    minlength: 5,
-    maxlength: 255
-  },
-  genre: { 
-    type: genreSchema,  
-    required: true
-  },
-  numberInStock: { 
-    type: Number, 
-    required: true,
-    min: 0,
-    max: 255
-  },
-  dailyRentalRate: { 
-    type: Number, 
-    required: true,
-    min: 0,
-    max: 255
-  }
-}));
+const Movie = mongoose.model(
+  "Movies",
+  new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    genre: {
+      type: genreSchema,
+      required: true,
+    },
+    numberInStock: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255,
+    },
+    dailyRentalRate: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255,
+    },
+  })
+);
 
 function validateMovie(movie) {
-  const schema = {
+  const schema = Joi.object({
     title: Joi.string().min(5).max(50).required(),
     genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).required(),
-    dailyRentalRate: Joi.number().min(0).required()
-  };
+    dailyRentalRate: Joi.number().min(0).required(),
+  });
 
-  return Joi.validate(movie, schema);
+  return schema.validate(movie);
 }
 
-exports.Movie = Movie; 
+exports.Movie = Movie;
 exports.validate = validateMovie;
