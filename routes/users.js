d04e480c4b5503170f369d8ad35f68ad264dc66e
Arpit Kumar <arpitk194@gmail.com>
2022-01-02 20:01:25 +0530

    added requireAuth & header visible logic

--- routes/users.js
+++ routes/users.js
@@ -25,6 +25,7 @@ router.post("/", async (req, res) => {
   const token = user.generateAuthToken();
   res
     .header("x-auth-token", token)
+    .header("access-control-expose-headers", "x-auth-token")
     .send(_.pick(user, ["_id", "name", "email"]));
 });
 
